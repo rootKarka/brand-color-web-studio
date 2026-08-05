42a9393f3613a208648afa8e7822b98ed5e03fe2
RootKarka <patrick.pajares@tecsup.edu.pe>
2026-08-05 18:12:22 -0500

    Modulo zonas/ completado al 100%

--- backend/zonas/serializers.py
+++ backend/zonas/serializers.py
@@ -1,8 +1,12 @@
 from rest_framework import serializers
 from .models import Zona
 
-
 class ZonaSerializer(serializers.ModelSerializer):
+
+    total_jefes = serializers.IntegerField(
+        read_only=True
+    )
+
     class Meta:
         model = Zona
         fields = "__all__"
--- backend/zonas/views.py
+++ backend/zonas/views.py
@@ -1,9 +1,25 @@
 from rest_framework import viewsets
+from django.db.models import Count, Q
+from common.choices import RolUsuario
 
 from .models import Zona
 from .serializers import ZonaSerializer
 
-
 class ZonaViewSet(viewsets.ModelViewSet):
     serializer_class = ZonaSerializer
-    queryset = Zona.objects.filter(is_active=True)
+
+    queryset = (
+        Zona.objects
+        .annotate(
+            total_jefes=Count(
+                "asignaciones_usuario",
+                filter=Q(
+                    asignaciones_usuario__is_active=True,
+                    asignaciones_usuario__usuario__is_active=True,
+                    asignaciones_usuario__usuario__rol=RolUsuario.JEFE_ANEXO,
+                ),
+                distinct=True,
+            )
+        )
+        .order_by("-is_active", "nombre")
+    )
